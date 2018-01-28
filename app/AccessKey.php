4a24c8872d6e4e0f6f7207fd5723d87910c8d11a
Jun Zheng <me@jackzh.com>
2018-01-28 11:10:45 -0500

    Added AccessKey model

---
+++ app/AccessKey.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccessKey extends Model
+{
+    public static function genRandomStr($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+        $str = '';
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+        return $str;
+    }
+}
