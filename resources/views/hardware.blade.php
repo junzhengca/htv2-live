d715937c9f3b3ceadcee20cc95d6fc48a711e888
Jun Zheng <me@jackzh.com>
2018-02-14 13:37:08 -0500

    Added hardware page

---
+++ resources/views/hardware.blade.php
@@ -0,0 +1,8 @@
+@extends('app.layout')
+
+@section('title', 'Home')
+
+@section('content')
+<h2>hardware</h2>
+<p class="large-description">Hardware list is not yet available, please check back soon.</p>
+@endsection
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -14,6 +14,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="page-2-nav"><a href="/"><span class="typcn typcn-home"></span> HOME</a></li>
+        <li class="page-2-nav"><a href="hardware"><span class="typcn typcn-cog"></span> HARDWARE</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
