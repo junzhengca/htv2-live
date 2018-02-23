b8078fa881c1a35caa104667e2c8e6f9e6e27a50
Jun Zheng <me@jackzh.com>
2018-02-22 21:09:32 -0500

    Fixed

--- resources/views/500hash.blade.php
+++ resources/views/500hash.blade.php
@@ -6,7 +6,7 @@
     <h2>500 encrypt</h2>
     <p class="large-description">Enter a string below to retrieve a letter</p>
     <input class="form-control" placeholder="String" id="hash-box" /><br>
-    <button class="btn btn-primary" id="get-button"><span class="typcn typcn-location-arrow"></span> Submit</button>
+    <button class="btn btn-primary" id="get-button">Submit</button>
     <h3>how to play</h3>
     <p class="how-to-play">
         <b>1.</b> You will receive a unique string of letters (encrypted string) upon check-in.<br>
@@ -21,11 +21,14 @@
 
     <script>
         $("#get-button").click(function(){
+            $(this).val("Checking...");
             axios.get("https://live.hackvalley2.com/api/hashletter/" + $("#hash-box").val()).then(function(data){
                 alert("The letter is " + data.data);
+                $("#get-button").html("Submit");
                 window.location.reload();
             }).catch(function(e){
                 console.log(e);
+                $("#get-button").html("Submit");
                 alert("Hash you entered is not valid. :(");
             })
         })
